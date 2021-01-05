a5781663f6c394cb5adf1007fe8eeed5d7382ceb
Zhivko Draganov <zhivko.draganov@mariadb.com>
2021-01-05 11:57:07 +0200

    Remove MariaDB specific stuff

--- provision.sh
+++ provision.sh
@@ -5,6 +5,6 @@ set -e
 DOTFILES_DIR=$HOME/dotfiles
 
 echo "Copy needed files to $HOME"
-ln -s $DOTFILES_DIR/.gitconfig $DOTFILES_DIR/.gitmessage $DOTFILES_DIR/.vimrc $DOTFILES_DIR/.vimrc.after $DOTFILES_DIR/.zshrc $DOTFILES_DIR/.path $HOME
+ln -s $DOTFILES_DIR/.gitconfig $DOTFILES_DIR/.gitmessage $DOTFILES_DIR/.vimrc $DOTFILES_DIR/.vimrc.after $DOTFILES_DIR/.zshrc $DOTFILES_DIR/.path $DOTFILES_DIR/.tmux.conf $HOME
 
 eval $SHELL
